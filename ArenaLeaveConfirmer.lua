0e80526acc599ec2736b43b08759d6fc73ad3c0f
AlexFolland <lexlexlex@gmail.com>
2015-06-20 02:23:40 -0700

    hopefully fix taint issue when trying to leave queue

--- ArenaLeaveConfirmer.lua
+++ ArenaLeaveConfirmer.lua
@@ -1,8 +1,16 @@
---local origConfirmOrLeaveBattlefield = ConfirmOrLeaveBattlefield;
+--to do: make this whole function never taint
+--idea: check the values of IsActiveBattlefieldArena() after the match has ended but the player is still in the arena
+--idea 2: use hooksecurefunc() to hook ConfirmOrLeaveBattlefield() then hide the popup
+--hooksecurefunc("ConfirmOrLeaveBattlefield", myConfirmOrLeaveBattlefield)
+--local origConfirmOrLeaveBattlefield = ConfirmOrLeaveBattlefield
+
+--okay, forget all that above.  this probably works.  test test test!
 ConfirmOrLeaveBattlefield = function()
-	if select(1, IsActiveBattlefieldArena()) then
-		LeaveBattlefield();
-	else
-		StaticPopup_Show("CONFIRM_LEAVE_BATTLEFIELD"); --origConfirmOrLeaveBattlefield();
+	local inArena, isRated = IsActiveBattlefieldArena()
+	if inArena then
+		LeaveBattlefield()
+		return
+	elseif not GetBattlefieldWinner() then
+		StaticPopup_Show("CONFIRM_LEAVE_BATTLEFIELD")
 	end
 end
