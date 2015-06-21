3d29db11d49f7292a00a55578818961d0c246df0
AlexFolland <lexlexlex@gmail.com>
2015-06-21 03:18:23 -0700

    comments and old code

--- ArenaLeaveConfirmer.lua
+++ ArenaLeaveConfirmer.lua
@@ -5,12 +5,34 @@
 --local origConfirmOrLeaveBattlefield = ConfirmOrLeaveBattlefield
 
 --okay, forget all that above.  this probably works.  test test test!
+local debug = true
 ConfirmOrLeaveBattlefield = function()
 	local inArena, isRated = IsActiveBattlefieldArena()
 	if inArena then
+		if debug then print("ArenaLeaveConfirmer: inArena is true") end
 		LeaveBattlefield()
-		return
-	elseif not GetBattlefieldWinner() then
-		StaticPopup_Show("CONFIRM_LEAVE_BATTLEFIELD")
 	end
 end
+	--[[
+	--check whether queued for arena to work around weird bug
+	local inArenaQueue = false
+	for i=1, GetMaxBattlefieldID() do
+		local status, mapName, teamSize, registeredMatch, suspend = GetBattlefieldStatus(i)
+		if teamSize > 0 then
+			local inArenaQueue = true
+		end
+	end
+	
+	local inArena, isRated = IsActiveBattlefieldArena()
+	if inArena then
+		if debug then print("ArenaLeaveConfirmer: inArena is true") end
+		LeaveBattlefield()
+		return
+	elseif not inArenaQueue then
+		if debug then print("ArenaLeaveConfirmer: not in arena queue") end
+		StaticPopup_Show("CONFIRM_LEAVE_BATTLEFIELD")
+		return
+	end
+	if debug then print("ArenaLeaveConfirmer: got to the end, so queued for arena and not in arena") end
+end
+--]]
