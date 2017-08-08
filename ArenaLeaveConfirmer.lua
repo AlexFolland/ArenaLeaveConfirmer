71a529fcb6489376704634b5b3a6ac1ef75b5a94
AlexFolland <lexlexlex@gmail.com>
2017-08-08 00:17:00 -0700

    confirm surrender too

--- ArenaLeaveConfirmer.lua
+++ ArenaLeaveConfirmer.lua
@@ -1,5 +1,9 @@
-hooksecurefunc(StaticPopupDialogs["CONFIRM_LEAVE_BATTLEFIELD"],"OnShow",function(self)
-	if IsActiveBattlefieldArena() or GetBattlefieldWinner() then
-		self.button1:Click()
-	end
-end)
+local function ArenaLeaveConfirm(self)
+    if IsActiveBattlefieldArena() or GetBattlefieldWinner() then
+        self.button1:Click()
+    end
+end
+
+hooksecurefunc(StaticPopupDialogs["CONFIRM_LEAVE_BATTLEFIELD"],"OnShow",ArenaLeaveConfirm)
+
+hooksecurefunc(StaticPopupDialogs["CONFIRM_SURRENDER_ARENA"],"OnShow",ArenaLeaveConfirm)
